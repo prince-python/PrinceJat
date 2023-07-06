baa07f3367b9c0e41f2ee23bf06bb449320a19ce
kali <kali@kali>
2023-07-06 20:44:28 +0530

    update commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,58 +15,6 @@ const home = () => {
      
 
       
-       {/* <div id="hovertext" className=' top-50 position-relative text-white border-3 border-dark d-inline-block mx-1'
-        
-         data-aos="fade-up"
-
-     data-aos-easing="ease-in-back"
-     data-aos-delay="100"
-     data-aos-offset="40">
-        <h1  className='text-white'  style={{fontFamily:"Lilita One",fontWeight:"bolder",textShadow:"0px 0px 100px blue" }}
-        >WELCOME</h1></div>
-
-
-
-        <div id="hovertext" className=' position-relative top-50  text-white border-3 border-dark d-inline-block mx-1' 
-         data-aos="fade-down"
-     data-aos-easing="ease-in-back"
-     data-aos-delay="400"
-     data-aos-offset="40">
-        <h1 className='text- font-weight-bold'  style={{fontFamily:"Lilita One",fontWeight:"bolder" }}> TO </h1></div>
-
-
-
-        <div id="hovertext" className=' position-relative  top-50 text-white border-3 border-dark d-inline-block mx-1'
-          data-aos="fade-up"
-     data-aos-easing="ease-in-back"
-     data-aos-delay="600"
-     data-aos-offset="40">
-        <h1 className='text-white font-weight-bold'  style={{fontFamily:"Lilita One",fontWeight:"bolder" }}> MY </h1></div>
-
-
-
-
-        <div id="hovertext" className=' position-relative  top-50  text-white border-3 border-dark d-inline-block mx-1'
-          data-aos="fade-down"
-     data-aos-easing="ease-in-back"
-     data-aos-delay="800"
-     data-aos-offset="40">
-        <h1 className='text-white font-weight-bold'  style={{fontFamily:"Lilita One",fontWeight:"bolder" }}> MODDED </h1></div>
-
-
-
-
-
-        <div id="hovertext" className=' position-relative top-50   text-white border-3 border-dark d-inline-block ' 
-        data-aos="fade-up"
-     data-aos-easing="ease-in-back"
-     data-aos-delay="1000"
-     data-aos-offset="200">
-        <h1 className='text-white font-weight-bold'  style={{fontFamily:"Lilita One",fontWeight:"bolder" }}>WORLD</h1></div>
-
-
-
- */}
 
 
       </div></center>
